44f2abcce8e9af32d3739e2ec607bdc107f65bf1
LuendellCarlos <luendellcarlinhos@gmail.com>
2025-03-28 19:52:54 -0300

    Create Cod1.c
    Mostrando endereço das variaveis

---
+++ Codigos-Em-Sala/Cod1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+
+ int a, b;
+
+ a = 5;
+
+ b = a;
+
+ a = 8;
+
+ printf("\n a = %d, b = %d", a, b);
+
+ a = b = 5;
+
+ a = 8;
+
+ printf("\n a = %d, b = %d", a, b);
+ printf("\n a = %d, (%d), b = %d (%d)", a, &a, b, &b);
+
+
+return 0
