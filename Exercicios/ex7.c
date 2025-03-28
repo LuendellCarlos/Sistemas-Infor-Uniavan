1b507d91b53a012d34e5d5ee7317bdc81fe71ebb
LuendellCarlos <luendellcarlinhos@gmail.com>
2025-03-28 16:13:26 -0300

    Create ex7.c
    Leia dois valores para as variáveis A e B, e efetue a troca dos valores de forma que a variável A passe a ter o valor da variável B e que a variável B passe a ter o valor da variável A. Ao final, exiba os resultados. Obs.: deverão ser feitos dois algoritmos: no primeiro poderá ser usada uma terceira variável. No segundo somente as variáveis A e B.

---
+++ Exercicios/ex7.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+//7. Leia dois valores para as variáveis A e B, e efetue a troca dos valores de forma que a variável A passe a ter o valor da variável B e que a variável B passe a ter o valor da
+// variável A. Ao final, exiba os resultados. Obs.: deverão ser feitos dois algoritmos: noprimeiro poderá ser usada uma terceira variável. No segundo somente as variáveis A e B.
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+
+   int a, b, c, d, troca;
+
+   printf("Primeira variável: ");
+   scanf("%d", &a);
+
+   printf("Segunda variável: ");
+   scanf("%d", &b);
+
+   c = a;
+   d = b;
+
+   troca = a;
+   a = b;
+   b = troca;
+
+   printf("\nPrimeiro método: A = %d \t B = %d\n ");
+
+   c = c + d;
+   d = c - d;
+   c = c - d;
+
+   printf("\nSegundo método: A = %d \t B = %d\n", c, d);
+
+
+    return 0;
+}
