b7fe9b7fc7711bf46b627b39631392aa3463c2ae
LuendellCarlos <luendellcarlinhos@gmail.com>
2025-03-28 21:23:52 -0300

    sizeof.c

--- Codigos-Em-Sala/sizeof.c
+++ Codigos-Em-Sala/sizeof.c
@@ -12,8 +12,8 @@ int main(void)
 
     printf("O tamanho da variável Lulu é %u bytes\n", sizeof(Lulu));
 
-    double brassanini = 3.14;
-    printf("O tamanho da variável brassanini é %u bytes\n", sizeof(brassanini));
+    double carlinhos = 3.14;
+    printf("O tamanho da variável carlinhos é %u bytes\n", sizeof(carlinhos));
 
     char carro = 'nissan';
     printf("O tamanho da variável carro é %u bytes\n", sizeof(carro));
